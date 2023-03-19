76386eb019d6c55fb836ed554291685b7bd6008c
Mbyron26 <Mbyron26@outlook.com>
2023-03-19 08:45:21 +0800

    Updated OPT.

--- MbyronModsCommon/MbyronModsCommon/OptionPanel/OptionPanelTool.cs
+++ MbyronModsCommon/MbyronModsCommon/OptionPanel/OptionPanelTool.cs
@@ -6,9 +6,8 @@ using UnityEngine;
 namespace MbyronModsCommon {
     public static class OptionPanelTool {
         public static PropertyPanel Group { get; set; }
-        static OptionPanelTool() {
-            ModLogger.ModLog("Initialize option panel tool.");
-        }
+        public static RectOffset DefaultOffset => new(10, 10, 10, 10);
+        static OptionPanelTool() => ModLogger.ModLog("Initialize option panel tool.");
 
         public static PropertyPanel AddGroup(UIComponent parent, float width, string caption) {
             Group = parent.AddUIComponent<PropertyPanel>();
@@ -48,25 +47,23 @@ namespace MbyronModsCommon {
         public static UIPanel AddStringField(string majorText, string stringField, string minorText, out UILabel majorLabel, out UILabel minorLabel, out UITextField textField, float width = 724, float height = 30, RectOffset majorOffset = null, RectOffset minorOffset = null) {
             if (Group is null) {
                 ModLogger.ModLog("ControlPanelTools_Group is null.");
+                textField = null;
                 majorLabel = null;
                 minorLabel = null;
-                textField = null;
                 return null;
             }
             var panel = AddChildPanel();
             textField = CustomField.AddTextField(panel, stringField, width, height);
             textField.horizontalAlignment = UIHorizontalAlignment.Left;
+            majorLabel = null;
+            minorLabel = null;
             if (majorText is not null) {
-                majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f, CustomColor.White);
-            } else {
-                majorLabel = null;
+                majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f);
+                if (minorText is not null) {
+                    minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
+                }
             }
-            if (minorText is not null) {
-                minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
-            } else {
-                minorLabel = null;
-            }
-            Group.UITool = new UIStyleGamma(panel) { Child = textField, MajorLabel = majorLabel, MinorLabel = minorLabel, Padding = new(10, 10, 10, 10), Gap = 4 };
+            Group.UITool = new UIStyleGamma(panel, textField, majorLabel, minorLabel, DefaultOffset) { Gap = 4 };
             Group.UITool.RefreshLayout();
             Group.UITool = null;
             return panel;
@@ -75,24 +72,22 @@ namespace MbyronModsCommon {
         public static UIPanel AddField<TypeField, TypeValue>(string majorText, string minorText, TypeValue defaultValue, TypeValue minLimit, TypeValue maxLimit, out UILabel majorLabel, out UILabel minorLabel, out TypeField valueField, Action<TypeValue> callback = null, float fieldWidth = 100, float fieldHeight = 28, RectOffset majorOffset = null, RectOffset minorOffset = null) where TypeField : CustomValueFieldBase<TypeValue> where TypeValue : IComparable {
             if (Group is null) {
                 ModLogger.ModLog("ControlPanelTools_Group is null.");
+                valueField = null;
                 majorLabel = null;
                 minorLabel = null;
-                valueField = null;
                 return null;
             }
             var panel = AddChildPanel();
             valueField = CustomField.AddOptionPanelValueField<TypeField, TypeValue>(panel, defaultValue, minLimit, maxLimit, callback, fieldWidth, fieldHeight);
+            majorLabel = null;
+            minorLabel = null;
             if (majorText is not null) {
-                majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f, CustomColor.White);
-            } else {
-                majorLabel = null;
+                majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f);
+                if (minorText is not null) {
+                    minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
+                }
             }
-            if (minorText is not null) {
-                minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
-            } else {
-                minorLabel = null;
-            }
-            Group.UITool = new UIStyleAlpha(panel) { Child = valueField, MajorLabel = majorLabel, MinorLabel = minorLabel, Padding = new(10, 10, 10, 10), LabelGap = 4 };
+            Group.UITool = new UIStyleAlpha(panel, valueField, majorLabel, minorLabel, DefaultOffset) { LabelGap = 4 };
             Group.UITool.RefreshLayout();
             Group.UITool = null;
             return panel;
@@ -101,24 +96,22 @@ namespace MbyronModsCommon {
         public static UIPanel AddButton(string majorText, string minorText, string buttonText, float? buttonWidth, float buttonHeight, OnButtonClicked callback, out UILabel majorLabel, out UILabel minorLabel, out UIButton button, RectOffset majorOffset = null, RectOffset minorOffset = null) {
             if (Group is null) {
                 ModLogger.ModLog("ControlPanelTools_Group is null.");
+                button = null;
                 majorLabel = null;
                 minorLabel = null;
-                button = null;
                 return null;
             }
             var panel = AddChildPanel();
             button = CustomButton.AddClickButton(panel, buttonText, buttonWidth, buttonHeight, callback);
+            majorLabel = null;
+            minorLabel = null;
             if (majorText is not null) {
-                majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f, CustomColor.White);
-            } else {
-                majorLabel = null;
+                majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f);
+                if (minorText is not null) {
+                    minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
+                }
             }
-            if (minorText is not null) {
-                minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
-            } else {
-                minorLabel = null;
-            }
-            Group.UITool = new UIStyleAlpha(panel) { Child = button, MajorLabel = majorLabel, MinorLabel = minorLabel, Padding = new(10, 10, 10, 10), LabelGap = 4 };
+            Group.UITool = new UIStyleAlpha(panel, button, majorLabel, minorLabel, DefaultOffset) { LabelGap = 4 };
             Group.UITool.RefreshLayout();
             Group.UITool = null;
             return panel;
@@ -134,17 +127,15 @@ namespace MbyronModsCommon {
             }
             var panel = AddChildPanel();
             slider = CustomSlider.AddSliderGamma(panel, sliderSize, min, max, step, defaultVal, callback);
+            majorLabel = null;
+            minorLabel = null;
             if (majorText is not null) {
-                majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f, CustomColor.White);
-            } else {
-                majorLabel = null;
+                majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f);
+                if (minorText is not null) {
+                    minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
+                }
             }
-            if (minorText is not null) {
-                minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
-            } else {
-                minorLabel = null;
-            }
-            Group.UITool = new UIStyleGamma(panel) { Child = slider, MajorLabel = majorLabel, MinorLabel = minorLabel, Padding = new(10, 10, 10, 10), Gap = 4 };
+            Group.UITool = new UIStyleGamma(panel, slider, majorLabel, minorLabel, DefaultOffset) { Gap = 4 };
             Group.UITool.RefreshLayout();
             Group.UITool = null;
             return panel;
@@ -160,17 +151,15 @@ namespace MbyronModsCommon {
             }
             var panel = AddChildPanel();
             slider = CustomSlider.AddSliderAlpha(panel, sliderText, min, max, step, defaultVal, sliderSize, callback, min.ToString(), max.ToString(), new(0, 0, 6, 0));
+            majorLabel = null;
+            minorLabel = null;
             if (majorText is not null) {
-                majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f, CustomColor.White);
-            } else {
-                majorLabel = null;
+                majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f);
+                if (minorText is not null) {
+                    minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
+                }
             }
-            if (minorText is not null) {
-                minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
-            } else {
-                minorLabel = null;
-            }
-            Group.UITool = new UIStyleGamma(panel) { Child = slider, MajorLabel = majorLabel, MinorLabel = minorLabel, Padding = new(10, 10, 10, 10), Gap = 4 };
+            Group.UITool = new UIStyleGamma(panel, slider, majorLabel, minorLabel, DefaultOffset) { Gap = 4 };
             Group.UITool.RefreshLayout();
             Group.UITool = null;
             return panel;
@@ -184,14 +173,15 @@ namespace MbyronModsCommon {
                 return null;
             }
             var panel = AddChildPanel();
-            majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f, CustomColor.White);
-            if (minorText is not null) {
-                minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
-                Group.UITool = new UIStyleAlpha(panel) { MajorLabel = majorLabel, MinorLabel = minorLabel, Padding = new(10, 10, 10, 10), LabelGap = 4 };
-            } else {
-                minorLabel = null;
-                Group.UITool = new UIStyleAlpha(panel) { MajorLabel = majorLabel, Padding = new(10, 10, 10, 10), LabelGap = 4 };
+            majorLabel = null;
+            minorLabel = null;
+            if (majorText is not null) {
+                majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f);
+                if (minorText is not null) {
+                    minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
+                }
             }
+            Group.UITool = new UIStyleAlpha(panel, null, majorLabel, minorLabel, DefaultOffset) { LabelGap = 4 };
             Group.UITool.RefreshLayout();
             Group.UITool = null;
             return panel;
@@ -208,13 +198,15 @@ namespace MbyronModsCommon {
             var panel = AddChildPanel();
             majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f, CustomColor.White);
             dropDown = CustomDropDown.AddOPDropDown(panel, options, defaultSelection, dropDownWidth, dropDownHeight);
-            if (minorText is not null) {
-                minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
-                Group.UITool = new UIStyleAlpha(panel) { Child = dropDown, MajorLabel = majorLabel, MinorLabel = minorLabel, Padding = new(10, 10, 10, 10), LabelGap = 4 };
-            } else {
-                minorLabel = null;
-                Group.UITool = new UIStyleAlpha(panel) { Child = dropDown, MajorLabel = majorLabel, Padding = new(10, 10, 10, 10), LabelGap = 4 };
+            majorLabel = null;
+            minorLabel = null;
+            if (majorText is not null) {
+                majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f);
+                if (minorText is not null) {
+                    minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
+                }
             }
+            Group.UITool = new UIStyleAlpha(panel, dropDown, majorLabel, minorLabel, DefaultOffset) { LabelGap = 4 };
             Group.UITool.RefreshLayout();
             Group.UITool = null;
             return panel;
@@ -230,15 +222,15 @@ namespace MbyronModsCommon {
             }
             var panel = AddChildPanel();
             button = CustomButton.AddToggleButton(panel, isChecked, callback);
-            majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f, CustomColor.White);
-            majorLabel.name = "MajorLabel";
-            if (minorText is not null) {
-                minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
-                Group.UITool = new UIStyleAlpha(panel) { Child = button, MajorLabel = majorLabel, MinorLabel = minorLabel, Padding = new(10, 10, 10, 10), LabelGap = 4 };
-            } else {
-                minorLabel = null;
-                Group.UITool = new UIStyleAlpha(panel) { Child = button, MajorLabel = majorLabel, Padding = new(10, 10, 10, 10) };
+            majorLabel = null;
+            minorLabel = null;
+            if (majorText is not null) {
+                majorLabel = CustomLabel.AddLabel(panel, majorText, 10, majorOffset, 1f);
+                if (minorText is not null) {
+                    minorLabel = CustomLabel.AddLabel(panel, minorText, 10, minorOffset, 0.8f, CustomColor.OffWhite);
+                }
             }
+            Group.UITool = new UIStyleAlpha(panel, button, majorLabel, minorLabel, DefaultOffset) { LabelGap = 4 };
             Group.UITool.RefreshLayout();
             Group.UITool = null;
             return panel;
